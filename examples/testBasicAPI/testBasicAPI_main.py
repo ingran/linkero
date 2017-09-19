7f0452dcba6bb2f21b75c6e88b922bf980740e3a
Rubén de Celis Hernández <rubendecelis@ingran.es>
2017-09-19 12:10:36 +0200

    Fixed import references in testBasicAPI

--- examples/testBasicAPI/testBasicAPI_main.py
+++ examples/testBasicAPI/testBasicAPI_main.py
@@ -6,10 +6,10 @@ import linkero.core.gateway.gevent_service as gevent
 import linkero.core.gateway.waitress_service as waitress
 
 # 2) APIs developed to use with Linkero
-import examples.testBasicAPI.testBasicAPI
+import testBasicAPI
 
 # 3) Load desired APIs
-examples.testBasicAPI.testBasicAPI.loadTestBasicAPI()
+testBasicAPI.loadTestBasicAPI()
 
 # 4) Run Linkero
 linkero.run()              # Run with Werkzeug (not recommended for production environments)
