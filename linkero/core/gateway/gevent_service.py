5d48b049a15f7603ccbc932691bcf047da35eb0d
Rubén de Celis Hernández <rubendecelis@ingran.es>
2017-10-16 14:38:11 +0200

    Add access log option in gevent config

--- linkero/core/gateway/gevent_service.py
+++ linkero/core/gateway/gevent_service.py
@@ -10,8 +10,10 @@ def run(app):
     if linkero.config["SSL"]["activate"]:
         gevert_server = WSGIServer((linkero.config["host"]["ip"], linkero.config["host"]["port"]), app,
                                    spawn=Pool(linkero.config["gevent"]["spawn"]),
+                                   log='default' if (linkero.config["gevent"]["accessLog"] == True) else None,
                                    keyfile=linkero.config["SSL"]["key"], certfile=linkero.config["SSL"]["certificate"])
     else:
         gevert_server = WSGIServer((linkero.config["host"]["ip"], linkero.config["host"]["port"]), app,
+                                   log='default' if (linkero.config["gevent"]["accessLog"] == True) else None,
                                    spawn=Pool(linkero.config["gevent"]["spawn"]))
     gevert_server.serve_forever()
